c0d057a6d9489e42bf232b389d5c0c28752c66e4
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-07-19 16:50:15 -0700

    adding onboarding assets to assets manager

--- lib/core/resources/assets_manager.dart
+++ lib/core/resources/assets_manager.dart
@@ -4,4 +4,8 @@ abstract class AssetsManager {
   // splash view assets
   static const appLogo = '$imagePath/app_logo.png';
   static const splashCurve = '$imagePath/splash_curve.png';
+
+  // onboarding view assets
+  static const onboardingCurve = '$imagePath/onboarding_curves.png';
+  static const onboardingPerson = '$imagePath/onboarding_person.png';
 }
