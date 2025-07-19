d9b967c184cf5149bd4e5d57831b5d52c97d6c0a
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-07-19 02:22:46 -0700

    start of building on boarding view

---
+++ lib/features/onboarding/presentation/widgets/onboarding_view_body.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class OnboardingViewBody extends StatelessWidget {
+  const OnboardingViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('You are in On Boarding now')),
+    );
+  }
+}
