f18c5cf8f058e533136560e4f34eb8dffc11cfd1
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-07-19 02:22:16 -0700

    splash view done

--- lib/core/resources/color_manager.dart
+++ lib/core/resources/color_manager.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class ColorManager {
   static const Color primaryColor = Color(0xff1A1A1A);
+  static const Color black = Color(0xff000000);
   static const Color white = Color(0xffFFFFFF);
   static const Color error = Color(0xffED1010);
   static const Color lightGrey = Color(0xff999999);
--- lib/features/splash/presentation/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/widgets/splash_view_body.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:styla/core/resources/assets_manager.dart';
+import 'package:styla/core/resources/color_manager.dart';
+import 'package:styla/core/resources/routes_manager.dart';
+import 'package:styla/core/resources/values_manager.dart';
+import 'package:styla/core/widgets/custom_loading_indicator.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
   @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToOnboarding();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('You are in splash view now')));
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: ColorManager.black,
+        body: Center(
+          child: Column(
+            children: [
+              Image.asset(AssetsManager.splashCurve),
+              Image.asset(AssetsManager.appLogo),
+              const SizedBox(height: AppSize.s200),
+              const CustomLoadingIndicator(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _navigateToOnboarding() {
+    Future.delayed(const Duration(seconds: 4), () {
+      GoRouter.of(context).pushReplacement(RoutesManager.onBoardingRoute);
+    });
   }
 }
