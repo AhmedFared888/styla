77706374d0bb98d9a33dc3732dfc58db315dc665
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-07-19 02:21:05 -0700

    adding image of splash curves to assets manager

--- lib/core/resources/assets_manager.dart
+++ lib/core/resources/assets_manager.dart
@@ -3,4 +3,5 @@ const String imagePath = 'assets/images';
 abstract class AssetsManager {
   // splash view assets
   static const appLogo = '$imagePath/app_logo.png';
+  static const splashCurve = '$imagePath/splash_curve.png';
 }
