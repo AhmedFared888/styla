661a73647e19099c500c3af11dcff347ea97609b
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-07-19 16:50:57 -0700

    creating Strings manager && adding onboarding strings

---
+++ lib/core/resources/strings_manager.dart
@@ -0,0 +1,5 @@
+abstract class StringsManager {
+  // on boarding strings
+  static String onboardingTitle = 'Define yourself in your unique way.';
+  static String onboardingButton = 'Get started';
+}
