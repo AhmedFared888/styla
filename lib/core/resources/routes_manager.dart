80e2b542e04debd78a8c9695799c2fcb57dab39d
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-07-20 15:36:31 -0700

    adding register && login routes in routes manager

--- lib/core/resources/routes_manager.dart
+++ lib/core/resources/routes_manager.dart
@@ -1,10 +1,14 @@
 import 'package:go_router/go_router.dart';
+import 'package:styla/features/auth/login/presentation/views/login_view.dart';
+import 'package:styla/features/auth/register/presentation/views/register_view.dart';
 import 'package:styla/features/onboarding/presentation/views/onboarding_view.dart';
 import 'package:styla/features/splash/presentation/views/splash_view.dart';
 
 class RoutesManager {
   static const String splashRoute = '/';
   static const String onBoardingRoute = '/onBoarding';
+  static const String registerRoute = '/register';
+  static const String loginRoute = '/login';
 
   static final router = GoRouter(
     routes: [
@@ -16,6 +20,11 @@ class RoutesManager {
         path: onBoardingRoute,
         builder: (context, state) => const OnboardingView(),
       ),
+      GoRoute(
+        path: registerRoute,
+        builder: (context, state) => const RegisterView(),
+      ),
+      GoRoute(path: loginRoute, builder: (context, state) => const LoginView()),
     ],
   );
 }
