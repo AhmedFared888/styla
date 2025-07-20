341bde796a0f4b7c78416ba7331a743f888b3410
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-07-20 15:35:51 -0700

    addnig registre strings to strings manager

--- lib/core/resources/strings_manager.dart
+++ lib/core/resources/strings_manager.dart
@@ -2,4 +2,25 @@ abstract class StringsManager {
   // on boarding strings
   static String onboardingTitle = 'Define yourself in your unique way.';
   static String onboardingButton = 'Get started';
+
+  // register strings
+  static const String createAnAccount = 'Create an account';
+  static const String letsCreateYourAccount = 'Let’s create your account.';
+  static const String fullName = 'Full Name';
+  static const String enterYourFullName = 'Enter your full name';
+  static const String email = 'Email';
+  static const String emailAdress = 'email address';
+  static const String enterYourEmailAdress = 'Enter your email address';
+  static const String password = 'Password';
+  static const String enterYourPassword = 'Enter your password';
+
+  static const String agreeingText = 'By signing up you agree to our ';
+  static const String terms = 'Terms, ';
+  static const String privacy = 'Privacy ';
+  static const String policy = 'Policy, ';
+  static const String and = 'and ';
+  static const String cookieUse = 'Cookie Use';
+  static const String createAnAccountButton = 'Create An Account';
+  static const String signUpwithGoogle = 'Sign Up with Google';
+  static const String signUpwithFacebook = 'Sign Up with Facebook';
 }
