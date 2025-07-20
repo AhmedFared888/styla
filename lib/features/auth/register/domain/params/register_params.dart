6bcd6d0aa76ad97ae3424d11239151609212c95e
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-07-20 16:08:13 -0700

    creating Register Params

---
+++ lib/features/auth/register/domain/params/register_params.dart
@@ -0,0 +1,11 @@
+class RegisterParams {
+  final String fullName;
+  final String email;
+  final String password;
+
+  RegisterParams({
+    required this.fullName,
+    required this.email,
+    required this.password,
+  });
+}
