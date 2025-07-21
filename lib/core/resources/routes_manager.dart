f7bc14f91f29207560b4b55f8c7857350a04c923
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-07-21 05:54:03 -0700

    adding BlocProvider for login view

--- lib/core/resources/routes_manager.dart
+++ lib/core/resources/routes_manager.dart
@@ -1,7 +1,9 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:styla/core/utils/functions/service_locator.dart';
+import 'package:styla/features/auth/domain/usecases/login_usecase.dart';
 import 'package:styla/features/auth/domain/usecases/register_usecase.dart';
+import 'package:styla/features/auth/presentation/login/manager/cubit/login_cubit.dart';
 import 'package:styla/features/auth/presentation/login/views/login_view.dart';
 import 'package:styla/features/auth/presentation/register/manager/cubit/register_cubit.dart';
 import 'package:styla/features/auth/presentation/register/views/register_view.dart';
@@ -33,7 +35,13 @@ class RoutesManager {
           child: const RegisterView(),
         ),
       ),
-      GoRoute(path: loginRoute, builder: (context, state) => const LoginView()),
+      GoRoute(
+        path: loginRoute,
+        builder: (context, state) => BlocProvider(
+          create: (context) => LoginCubit(getIt.get<LoginUsecase>()),
+          child: const LoginView(),
+        ),
+      ),
       GoRoute(path: homeRoute, builder: (context, state) => const HomeView()),
     ],
   );
