41a9bcee210e8028d66e47a70ced3bd15f09eb89
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-07-21 03:42:42 -0700

    adding home route

--- lib/core/resources/routes_manager.dart
+++ lib/core/resources/routes_manager.dart
@@ -5,6 +5,7 @@ import 'package:styla/features/auth/login/presentation/views/login_view.dart';
 import 'package:styla/features/auth/register/domain/usecases/register_usecase.dart';
 import 'package:styla/features/auth/register/presentation/manager/cubit/register_cubit.dart';
 import 'package:styla/features/auth/register/presentation/views/register_view.dart';
+import 'package:styla/features/home/presentation/views/home_view.dart';
 import 'package:styla/features/onboarding/presentation/views/onboarding_view.dart';
 import 'package:styla/features/splash/presentation/views/splash_view.dart';
 
@@ -13,6 +14,7 @@ class RoutesManager {
   static const String onBoardingRoute = '/onBoarding';
   static const String registerRoute = '/register';
   static const String loginRoute = '/login';
+  static const String homeRoute = '/home';
 
   static final router = GoRouter(
     routes: [
@@ -32,6 +34,7 @@ class RoutesManager {
         ),
       ),
       GoRoute(path: loginRoute, builder: (context, state) => const LoginView()),
+      GoRoute(path: homeRoute, builder: (context, state) => const HomeView()),
     ],
   );
 }
---
+++ lib/features/home/presentation/views/home_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/widgets.dart';
+import 'package:styla/features/home/presentation/widgets/home_view_body.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const HomeViewBody();
+  }
+}
