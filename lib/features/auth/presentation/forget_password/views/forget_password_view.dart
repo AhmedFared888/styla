af52287cb3523fae7c29dfb3507b5c3674d70c9a
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-07-22 01:46:24 -0700

    forget password ui done

--- lib/features/auth/presentation/forget_password/views/forget_password_view.dart
+++ lib/features/auth/presentation/forget_password/views/forget_password_view.dart
@@ -6,6 +6,6 @@ class ForgetPasswordView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const ForgetPasswordViewBody();
+    return ForgetPasswordViewBody();
   }
 }
--- lib/features/auth/presentation/forget_password/widgets/forget_password_view_body.dart
+++ lib/features/auth/presentation/forget_password/widgets/forget_password_view_body.dart
@@ -1,12 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:styla/core/resources/color_manager.dart';
+import 'package:styla/core/resources/strings_manager.dart';
+import 'package:styla/core/resources/styles_manager.dart';
+import 'package:styla/core/resources/values_manager.dart';
+import 'package:styla/core/widgets/custom_elevated_button.dart';
+import 'package:styla/core/widgets/custom_text_form_feild.dart';
 
 class ForgetPasswordViewBody extends StatelessWidget {
-  const ForgetPasswordViewBody({super.key});
+  ForgetPasswordViewBody({super.key});
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailTextEditingController =
+      TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('You are in forget password view now')),
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: AppSize.s24),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  StringsManager.forgotPassword,
+                  style: StylesManager.textStyle32Sem(
+                    ColorManager.primaryColor,
+                  ),
+                ),
+                const SizedBox(height: AppSize.s8),
+                Text(
+                  StringsManager.ressetPasswordText,
+                  style: StylesManager.textStyle16Reg(ColorManager.grey),
+                ),
+                const SizedBox(height: AppSize.s24),
+                Text(
+                  StringsManager.email,
+                  style: StylesManager.textStyle16Med(
+                    ColorManager.primaryColor,
+                  ),
+                ),
+                const SizedBox(height: AppSize.s6),
+                CustomTextFormField(
+                  textEditingController: _emailTextEditingController,
+                  hintText: StringsManager.emailAdress,
+                  labelText: StringsManager.enterYourEmailAdress,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Email cannot be empty';
+                    } else if (!value.contains('@')) {
+                      return 'Enter a valid email';
+                    }
+                    return null;
+                  },
+                ),
+                const Spacer(),
+                CustomElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {}
+                  },
+                  title: StringsManager.send,
+                ),
+                const SizedBox(height: AppSize.s24),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
