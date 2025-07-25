e6d6988e57f6788964036ab174476f4e3b483000
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-07-25 16:30:09 -0700

    home view UI done

--- lib/features/home/presentation/widgets/home_view_body.dart
+++ lib/features/home/presentation/widgets/home_view_body.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:styla/core/resources/color_manager.dart';
-import 'package:styla/core/resources/styles_manager.dart';
+import 'package:styla/core/resources/values_manager.dart';
+import 'package:styla/features/home/presentation/widgets/category_listview.dart';
+import 'package:styla/features/home/presentation/widgets/home_gridview.dart';
+import 'package:styla/features/home/presentation/widgets/search_section.dart';
+import 'package:styla/features/home/presentation/widgets/title_section.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(
-          'Home',
-          style: StylesManager.textStyle20Sem(ColorManager.primaryColor),
+    return const Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: AppSize.s24),
+          child: Column(
+            children: [
+              TitleSection(),
+              SizedBox(height: AppSize.s8),
+              SearchSection(),
+              SizedBox(height: AppSize.s16),
+              CategoryListView(),
+              SizedBox(height: AppSize.s24),
+              HomeGridView(),
+            ],
+          ),
         ),
       ),
     );
