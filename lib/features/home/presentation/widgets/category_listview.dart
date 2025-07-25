f8db8f2be60b472ba38e7771c49d216eecb14ad4
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-07-25 16:14:07 -0700

    adding category listview

---
+++ lib/features/home/presentation/widgets/category_listview.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:styla/core/resources/values_manager.dart';
+import 'package:styla/features/home/presentation/widgets/category_listview_item.dart';
+
+class CategoryListView extends StatelessWidget {
+  const CategoryListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: AppSize.s40,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) {
+          return CategoryListViewItem();
+        },
+        itemCount: 10,
+      ),
+    );
+  }
+}
