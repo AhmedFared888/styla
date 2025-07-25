e76fe9c1d340bcab95990d08a1d6336b9cc43335
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-07-25 16:25:40 -0700

    adding title and notification button

---
+++ lib/features/home/presentation/widgets/title_section.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:styla/core/resources/assets_manager.dart';
+import 'package:styla/core/resources/color_manager.dart';
+import 'package:styla/core/resources/routes_manager.dart';
+import 'package:styla/core/resources/strings_manager.dart';
+import 'package:styla/core/resources/styles_manager.dart';
+import 'package:styla/core/resources/values_manager.dart';
+
+class TitleSection extends StatelessWidget {
+  const TitleSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          StringsManager.discover,
+          style: StylesManager.textStyle32Sem(ColorManager.primaryColor),
+        ),
+        const Spacer(),
+        GestureDetector(
+          onTap: () {
+            GoRouter.of(context).push(RoutesManager.notificationRoute);
+          },
+          child: Image.asset(AssetsManager.notificationIcon),
+        ),
+        const SizedBox(width: AppSize.s8),
+      ],
+    );
+  }
+}
