d8dd9fcdcb3c5c9cd5090284470d4c3ae0961a58
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-07-27 01:19:10 -0700

    generate category_entity.g.dart and register it in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:styla/core/resources/routes_manager.dart';
 import 'package:styla/core/resources/theme_manager.dart';
 import 'package:styla/core/utils/functions/service_locator.dart';
+import 'package:styla/features/home/domain/entities/category_entity.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
+  await Hive.initFlutter();
+  Hive.registerAdapter(CategoryEntityAdapter());
 
   setupServiceLocator();
 
