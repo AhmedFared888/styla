69b72757806e3cbccf25c5aff9054164ede7f63d
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-07-27 01:43:40 -0700

    home repo impl

---
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -0,0 +1,36 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:styla/core/errors/failure.dart';
+import 'package:styla/features/home/data/data_sources/home_local_data_source.dart';
+import 'package:styla/features/home/data/data_sources/home_remote_data_source.dart';
+import 'package:styla/features/home/domain/entities/category_entity.dart';
+import 'package:styla/features/home/domain/repos/home_repo.dart';
+
+class HomeRepoImpl extends HomeRepo {
+  final HomeRemoteDataSource homeRemoteDataSource;
+  final HomeLocalDataSource homeLocalDataSource;
+
+  HomeRepoImpl({
+    required this.homeRemoteDataSource,
+    required this.homeLocalDataSource,
+  });
+  @override
+  Future<Either<Failure, List<CategoryEntity>>> getAllCategory() async {
+    try {
+      var categoryLocal = homeLocalDataSource.getAllCategory();
+      if (categoryLocal.isNotEmpty) {
+        return right(categoryLocal);
+      }
+      var categories = await homeRemoteDataSource.getAllCategory();
+      return right(categories);
+    } catch (e, s) {
+      print(e);
+      print(s);
+      if (e is DioException) {
+        return left(ServerFailre.fromDioException(e));
+      } else {
+        return left(ServerFailre(e.toString()));
+      }
+    }
+  }
+}
