8e6a935342b44ff582e83a8fd0e443fec1766ceb
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-07-29 20:12:58 -0700

    Adding CategoryCubit BlocProvider with injected HomeUsecase

--- lib/features/home/presentation/widgets/category_listview.dart
+++ lib/features/home/presentation/widgets/category_listview.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:styla/core/resources/values_manager.dart';
+import 'package:styla/core/utils/functions/service_locator.dart';
+import 'package:styla/features/home/domain/usecases/home_usecase.dart';
+import 'package:styla/features/home/presentation/manager/cubit/category_cubit.dart';
 import 'package:styla/features/home/presentation/widgets/category_listview_item.dart';
 
 class CategoryListView extends StatelessWidget {
@@ -7,14 +11,17 @@ class CategoryListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: AppSize.s40,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) {
-          return const CategoryListViewItem();
-        },
-        itemCount: 10,
+    return BlocProvider(
+      create: (context) => CategoryCubit(getIt.get<HomeUsecase>()),
+      child: SizedBox(
+        height: AppSize.s40,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return const CategoryListViewItem();
+          },
+          itemCount: 10,
+        ),
       ),
     );
   }
