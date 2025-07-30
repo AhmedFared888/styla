a6d0dc1e6084f0731ca38bfca8cd92bd8b3f4fdd
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-07-29 21:14:05 -0700

    update HomeRepo with getAllProducts

--- lib/features/home/domain/repos/home_repo.dart
+++ lib/features/home/domain/repos/home_repo.dart
@@ -1,7 +1,9 @@
 import 'package:dartz/dartz.dart';
 import 'package:styla/core/errors/failure.dart';
 import 'package:styla/features/home/domain/entities/category_entity/category_entity.dart';
+import 'package:styla/features/home/domain/entities/product_entity/product_entity.dart';
 
 abstract class HomeRepo {
   Future<Either<Failure, List<CategoryEntity>>> getAllCategory();
+  Future<Either<Failure, List<ProductEntity>>> getAllProducts();
 }
