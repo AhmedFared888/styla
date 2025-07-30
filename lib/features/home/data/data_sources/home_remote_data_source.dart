5f7c8f78be6f982eef4a494382d9e2537fff9534
AhmedFared888 <ahmedfared3404814@gmai.com>
2025-07-29 21:20:00 -0700

    create product model

--- lib/features/home/data/data_sources/home_remote_data_source.dart
+++ lib/features/home/data/data_sources/home_remote_data_source.dart
@@ -1,7 +1,7 @@
 import 'package:styla/constants.dart';
 import 'package:styla/core/utils/api_service.dart';
 import 'package:styla/core/utils/functions/save_category_data.dart';
-import 'package:styla/features/home/data/models/category_model.dart';
+import 'package:styla/features/home/data/models/category_model/category_model.dart';
 import 'package:styla/features/home/domain/entities/category_entity/category_entity.dart';
 
 abstract class HomeRemoteDataSource {
---
+++ lib/features/home/data/models/product_model/product_model.dart
@@ -0,0 +1,49 @@
+import 'package:styla/features/home/domain/entities/product_entity/product_entity.dart';
+
+import 'rating.dart';
+
+class ProductModel extends ProductEntity {
+  int? id;
+  String? title;
+  double? price;
+  String? description;
+  String? category;
+  String? image;
+  Rating? rating;
+
+  ProductModel({
+    this.id,
+    this.title,
+    this.price,
+    this.description,
+    this.category,
+    this.image,
+    this.rating,
+  }) : super(
+         productImage: image ?? 'There is no product',
+         productName: title ?? 'There is no product',
+         productPrice: price ?? 0.0,
+       );
+
+  factory ProductModel.fromJson(Map<String, dynamic> json) => ProductModel(
+    id: json['id'] as int?,
+    title: json['title'] as String?,
+    price: (json['price'] as num?)?.toDouble(),
+    description: json['description'] as String?,
+    category: json['category'] as String?,
+    image: json['image'] as String?,
+    rating: json['rating'] == null
+        ? null
+        : Rating.fromJson(json['rating'] as Map<String, dynamic>),
+  );
+
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'title': title,
+    'price': price,
+    'description': description,
+    'category': category,
+    'image': image,
+    'rating': rating?.toJson(),
+  };
+}
