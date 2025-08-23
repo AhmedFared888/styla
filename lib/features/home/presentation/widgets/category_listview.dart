9797cf863e9b3ef770752525388185d1b3e60ebe
AhmedFared44 <ahmed.fared@teleoceans.com>
2025-08-22 23:27:40 -0700

    removing bloc provider from here and added it to the main view to make connection between it and the rest of the screen

--- lib/features/home/presentation/widgets/category_listview.dart
+++ lib/features/home/presentation/widgets/category_listview.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:styla/core/resources/values_manager.dart';
-import 'package:styla/core/utils/functions/service_locator.dart';
 import 'package:styla/core/widgets/custom_loading_indicator.dart';
-import 'package:styla/features/home/domain/usecases/categories_usecase.dart';
-import 'package:styla/features/home/presentation/manager/cubit/category_cubit.dart';
+import 'package:styla/features/home/presentation/manager/category_cubit/category_cubit.dart';
 import 'package:styla/features/home/presentation/widgets/category_listview_item.dart';
 
 class CategoryListView extends StatelessWidget {
@@ -12,30 +10,26 @@ class CategoryListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) =>
-          CategoryCubit(getIt.get<CategoriesUsecase>())..getAllCategories(),
-      child: SizedBox(
-        height: AppSize.s40,
-        child: BlocBuilder<CategoryCubit, CategoryState>(
-          builder: (context, state) {
-            if (state is CategorySuccesse) {
-              return ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) {
-                  return CategoryListViewItem(
-                    category: state.categories[index],
-                  );
-                },
-                itemCount: state.categories.length,
-              );
-            } else if (state is CategoryFailure) {
-              return Center(child: Text(state.errorMessage));
-            } else {
-              return const CustomLoadingIndicator();
-            }
-          },
-        ),
+    return SizedBox(
+      height: AppSize.s40,
+      child: BlocBuilder<CategoryCubit, CategoryState>(
+        builder: (context, state) {
+          if (state is CategorySuccesse) {
+            return ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return CategoryListViewItem(
+                  category: state.categories[index],
+                );
+              },
+              itemCount: state.categories.length,
+            );
+          } else if (state is CategoryFailure) {
+            return Center(child: Text(state.errorMessage));
+          } else {
+            return const CustomLoadingIndicator();
+          }
+        },
       ),
     );
   }
--- lib/features/home/presentation/widgets/category_listview_item.dart
+++ lib/features/home/presentation/widgets/category_listview_item.dart
@@ -13,19 +13,28 @@ class CategoryListViewItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: AppSize.s4),
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(AppSize.s10),
-          border: Border.all(color: ColorManager.lightGrey, width: AppSize.s1),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: AppPadding.p20,
-            vertical: AppPadding.p8,
+      child: InkWell(
+        onTap: () {},
+        child: Container(
+          decoration: BoxDecoration(
+            color: ColorManager.white,
+            borderRadius: BorderRadius.circular(AppSize.s10),
+            border: Border.all(
+              color: ColorManager.lightGrey,
+              width: AppSize.s1,
+            ),
           ),
-          child: Text(
-            category.categoryName,
-            style: StylesManager.textStyle16Med(ColorManager.primaryColor),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: AppPadding.p20,
+              vertical: AppPadding.p8,
+            ),
+            child: Text(
+              category.categoryName,
+              style: StylesManager.textStyle16Med(
+                ColorManager.primaryColor,
+              ),
+            ),
           ),
         ),
       ),
