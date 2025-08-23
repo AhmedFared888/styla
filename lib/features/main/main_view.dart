9fe7f35e163cd54feb494a1511e3817d264dc76a
AhmedFared44 <ahmed.fared@teleoceans.com>
2025-08-22 23:13:47 -0700

    adding MultiBlocProvider && convert all icons to SVGs && add a safety check in the MainView to prevent crashes when the cubits are created

--- lib/features/main/main_view.dart
+++ lib/features/main/main_view.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:styla/core/resources/assets_manager.dart';
 import 'package:styla/core/resources/color_manager.dart';
 import 'package:styla/core/resources/strings_manager.dart';
+import 'package:styla/core/utils/functions/service_locator.dart';
 import 'package:styla/features/account/presentation/views/account_view.dart';
 import 'package:styla/features/cart/presentation/views/cart_view.dart';
+import 'package:styla/features/home/domain/usecases/allproducts_usecase.dart';
+import 'package:styla/features/home/domain/usecases/categories_usecase.dart';
+import 'package:styla/features/home/presentation/manager/all_product_cubit/all_product_cubit.dart';
+import 'package:styla/features/home/presentation/manager/category_cubit/category_cubit.dart';
 import 'package:styla/features/home/presentation/views/home_view.dart';
 import 'package:styla/features/saved/presentation/views/saved_view.dart';
 import 'package:styla/features/search/presentation/views/search_view.dart';
@@ -19,12 +26,41 @@ class _MainViewState extends State<MainView> {
   int _currentIndex = 0;
 
   final List<Widget> _views = [
-    const HomeView(),
+    MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) {
+            try {
+              final cubit = CategoryCubit(getIt.get<CategoriesUsecase>());
+              cubit.getAllCategories();
+              return cubit;
+            } catch (e) {
+              print("❌ Error creating CategoryCubit: $e");
+              return CategoryCubit(getIt.get<CategoriesUsecase>());
+            }
+          },
+        ),
+        BlocProvider(
+          create: (context) {
+            try {
+              final cubit = AllProductCubit(getIt.get<AllproductsUsecase>());
+              cubit.getAllProducts();
+              return cubit;
+            } catch (e) {
+              print("❌ Error creating AllProductCubit: $e");
+              return AllProductCubit(getIt.get<AllproductsUsecase>());
+            }
+          },
+        ),
+      ],
+      child: const HomeView(),
+    ),
     const SearchView(),
     const SavedView(),
     const CartView(),
     const AccountView(),
   ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,27 +69,73 @@ class _MainViewState extends State<MainView> {
         type: BottomNavigationBarType.fixed,
         selectedItemColor: ColorManager.primaryColor,
         unselectedItemColor: ColorManager.lightGrey,
+
         currentIndex: _currentIndex,
         onTap: (index) => setState(() => _currentIndex = index),
         items: [
-          const BottomNavigationBarItem(
-            icon: ImageIcon(AssetImage(AssetsManager.home)),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              AssetsManager.home,
+              fit: BoxFit.cover,
+              colorFilter: ColorFilter.mode(
+                _currentIndex == 0
+                    ? ColorManager.primaryColor
+                    : ColorManager.lightGrey,
+                BlendMode.srcIn,
+              ),
+            ),
             label: StringsManager.home,
           ),
-          const BottomNavigationBarItem(
-            icon: ImageIcon(AssetImage(AssetsManager.search)),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              AssetsManager.search,
+              fit: BoxFit.cover,
+              colorFilter: ColorFilter.mode(
+                _currentIndex == 1
+                    ? ColorManager.primaryColor
+                    : ColorManager.lightGrey,
+                BlendMode.srcIn,
+              ),
+            ),
             label: StringsManager.search,
           ),
-          const BottomNavigationBarItem(
-            icon: ImageIcon(AssetImage(AssetsManager.saved)),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              AssetsManager.favorite,
+              fit: BoxFit.cover,
+              colorFilter: ColorFilter.mode(
+                _currentIndex == 2
+                    ? ColorManager.primaryColor
+                    : ColorManager.lightGrey,
+                BlendMode.srcIn,
+              ),
+            ),
             label: StringsManager.saved,
           ),
-          const BottomNavigationBarItem(
-            icon: ImageIcon(AssetImage(AssetsManager.cart)),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              AssetsManager.cart,
+              fit: BoxFit.cover,
+              colorFilter: ColorFilter.mode(
+                _currentIndex == 3
+                    ? ColorManager.primaryColor
+                    : ColorManager.lightGrey,
+                BlendMode.srcIn,
+              ),
+            ),
             label: StringsManager.cart,
           ),
-          const BottomNavigationBarItem(
-            icon: ImageIcon(AssetImage(AssetsManager.account)),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              AssetsManager.account,
+              fit: BoxFit.cover,
+              colorFilter: ColorFilter.mode(
+                _currentIndex == 4
+                    ? ColorManager.primaryColor
+                    : ColorManager.lightGrey,
+                BlendMode.srcIn,
+              ),
+            ),
             label: StringsManager.account,
           ),
         ],
